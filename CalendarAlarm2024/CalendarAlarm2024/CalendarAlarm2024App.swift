47413061446e39420f978366cc0d8bb80d5a69f1
Gobooth.ca <dan@gobooth.ca>
2024-09-27 09:37:12 -0400

    First Commit
    empty state

---
+++ CalendarAlarm2024/CalendarAlarm2024/CalendarAlarm2024App.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAlarm2024App.swift
+//  CalendarAlarm2024
+//
+//  Created by Daniel Goneau on 2024-09-27.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAlarm2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarAlarm2024/CalendarAlarm2024Tests/CalendarAlarm2024Tests.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAlarm2024Tests.swift
+//  CalendarAlarm2024Tests
+//
+//  Created by Daniel Goneau on 2024-09-27.
+//
+
+import Testing
+@testable import CalendarAlarm2024
+
+struct CalendarAlarm2024Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
